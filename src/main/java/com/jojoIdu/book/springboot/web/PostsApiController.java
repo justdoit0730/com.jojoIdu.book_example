7cdbc31df3b50d206509605693ce7a70b11d830a
junhee <a07308@naver.com>
2023-10-15 18:10:05 +0900

    feat: 1.0.5-Chapter
    [Chapter 5]
    - git ignore 설정
    - google 로그인 기능

--- src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
-    
+
     private  final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
